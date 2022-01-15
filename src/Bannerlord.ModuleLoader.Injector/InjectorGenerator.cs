dc417306c5dbbbca0f8bde256abb1fb2897ff44c
Aragas <personal@aragas.org>
2022-01-15 03:17:22 +0300

    Added a simple Injector

---
+++ src/Bannerlord.ModuleLoader.Injector/InjectorGenerator.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.CodeAnalysis;
+
+using System.IO;
+using System.Linq;
+
+namespace Bannerlord.ModuleLoader.Injector
+{
+    [Generator]
+    public class InjectorGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context) { }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            if (!context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("build_property.outputpath", out var path))
+                return;
+
+            if (!context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("build_property.msbuildprojectfullpath", out var csPath))
+                return;
+
+            var fullPath = Path.Combine(Path.GetDirectoryName(csPath), path);
+
+            var name = context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("build_property.modulename", out var moduleName)
+                ? $"{moduleName}.Loader"
+                : $"{context.Compilation.Assembly.Name.Split('.').FirstOrDefault()}.Loader";
+
+            var resourceBase = "Bannerlord.ModuleLoader.Generator.Bannerlord.ModuleLoader";
+
+            using (var dllStream = typeof(InjectorGenerator).Assembly.GetManifestResourceStream($"{resourceBase}.dll"))
+            using (var fileStream = new FileStream(Path.Combine(fullPath, $"{name}.dll"), FileMode.Create, FileAccess.Write))
+            {
+                dllStream?.CopyTo(fileStream);
+            }
+
+            using (var pdbStream = typeof(InjectorGenerator).Assembly.GetManifestResourceStream($"{resourceBase}.pdb"))
+            using (var fileStream = new FileStream(Path.Combine(fullPath, $"{name}.pdb"), FileMode.Create, FileAccess.Write))
+            {
+                pdbStream?.CopyTo(fileStream);
+            }
+        }
+    }
+}
