54f55952ffd396489e251051b9f48cdf1ec1d2a3
Aragas <personal@aragas.org>
2022-01-18 15:24:59 +0300

    Using regex because we do the same in MSBuild

--- src/Bannerlord.ModuleLoader.Injector/InjectorGenerator.cs
+++ src/Bannerlord.ModuleLoader.Injector/InjectorGenerator.cs
@@ -5,6 +5,7 @@ using Mono.Cecil;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Bannerlord.ModuleLoader.Injector
 {
@@ -56,14 +57,18 @@ namespace Bannerlord.ModuleLoader.Injector
             using var modifiedAss = AssemblyDefinition.ReadAssembly(assemblyStream);
             modifiedAss.Name.Name = moduleName;
             var type = modifiedAss.MainModule.GetType("Bannerlord.ModuleLoader.SubModule");
-            var validClassName = new string(moduleName.Select((c, i) => IsValidInIdentifier(c, i == 0) ? c : '_').ToArray());
-            type.Name = validClassName;
+            var validModuleName = FlatNameRegex(moduleName);
+            type.Name = validModuleName;
 
             var ms = new MemoryStream();
             modifiedAss.Write(ms, new WriterParameters() { DeterministicMvid = true });
             ms.Seek(0, SeekOrigin.Begin);
             return ms;
         }
+
+        private static string FlatNameRegex(string str) => new Regex("[^a-zA-Z0-9]", RegexOptions.Compiled).Replace(str, "_");
+
+        private static string FlatName(string str) => new(str.Select((c, i) => IsValidInIdentifier(c, i == 0) ? c : '_').ToArray());
         private static bool IsValidInIdentifier(char c, bool firstChar = true) => char.GetUnicodeCategory(c) switch
         {
             // Always allowed in C# identifiers
